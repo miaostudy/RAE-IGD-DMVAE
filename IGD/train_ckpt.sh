f111d732b53b01895e0512273f61cdbf994cf02a
Meow <2775921733@qq.com>
2026-01-09 14:50:51 +0800

    Update train_ckpt.sh

--- IGD/train_ckpt.sh
+++ IGD/train_ckpt.sh
@@ -2,6 +2,6 @@
 model=convnet6
 spec=woof
 
-python igd/train_ckpts.py -d imagenet --imagenet_dir /data/wlf/datasets/imagenet/train/ /data/wlf/datasets/imagenet/ \
+python IGD/train_ckpts.py -d imagenet --imagenet_dir /data/wlf/datasets/imagenet/train/ /data/wlf/datasets/imagenet/ \
     -n ${model} --nclass 10 --norm_type instance --tag test --slct_type random --spec ${spec} --mixup vanilla --repeat 1 \
     --ckpt-dir ckpts/${spec}/${model}/ --epochs 50 --depth 6 --ipc -1
