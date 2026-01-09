8013a3f78af8dd7c53a14a7aad0f71b14f7837c0
Meow <2775921733@qq.com>
2026-01-09 14:50:14 +0800

    Update train_ckpt.sh

--- IGD/train_ckpt.sh
+++ IGD/train_ckpt.sh
@@ -4,4 +4,4 @@ spec=woof
 
 python train_ckpts.py -d imagenet --imagenet_dir /data/wlf/datasets/imagenet/train/ /data/wlf/datasets/imagenet/ \
     -n ${model} --nclass 10 --norm_type instance --tag test --slct_type random --spec ${spec} --mixup vanilla --repeat 1 \
-    --ckpt-dir ./ckpts/${spec}/${model}/ --epochs 50 --depth 6 --ipc -1
+    --ckpt-dir ckpts/${spec}/${model}/ --epochs 50 --depth 6 --ipc -1
